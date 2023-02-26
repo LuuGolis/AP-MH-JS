32782cffdb13325410b88a75b1ce8309766b1f2c
LuuGolis <golischevskyl@gmail.com>
2023-02-26 15:25:35 -0300

    Card dinámica past

---
+++ scripts/pastEvents.js
@@ -0,0 +1,23 @@
+const contenedorTarjetas = document.querySelector('#contenedorCards')
+
+let tarjetasCreadas = crearTarjetas(data.events)
+
+contenedorTarjetas.innerHTML = tarjetasCreadas
+
+function crearTarjetas(arrayDatos){
+    let tarjetas = ''
+    for(const event of arrayDatos){
+        if(event.date < data.currentDate){
+        tarjetas += `<div class="card" style="width: 15rem;">
+        <img src="${event.image}" class="card-img-top" alt="cardImg" width="150" height="150">
+        <div class="card-body">
+            <h5 class="card-title">${event.name}</h5>
+            <p class="card-text">${event.description}</p>
+            <p>$${event.price}</p>
+            <a href="details.html" class="btn btn-dark">See more</a>
+        </div>
+    </div> `
+}
+    }
+    return tarjetas
+}
