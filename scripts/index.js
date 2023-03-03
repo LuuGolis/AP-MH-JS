b47d533d6eb668d29d540bb441522f10f5840a4e
LuuGolis <golischevskyl@gmail.com>
2023-03-03 17:46:49 -0300

    Tarjetas dinámicas con Foreach en index

--- scripts/index.js
+++ scripts/index.js
@@ -5,20 +5,8 @@ let tarjetasCreadas = crearTarjetas(data.events)
 contenedorTarjetas.innerHTML = tarjetasCreadas
 
 function crearTarjetas(arrayDatos){
-    
-   /* for(const event of arrayDatos){
-        tarjetas += `<div class="card" style="width: 18rem; height:25rem;">
-        <img src="${event.image}" class="card-img-top" alt="cardImg" width="150" height="150">
-        <div class="card-body">
-            <h5 class="card-title">${event.name}</h5>
-            <p class="card-text">${event.description}</p>
-            <p>$${event.price}</p>
-            <a href="details.html" class="btn btn-dark" id="btn">See more</a>
-        </div>
-    </div> `
-    }
-    return tarjetas*/
-    let tarjetas = arrayDatos.array.forEach(event => { tarjeta += `<div class="card" style="width: 18rem; height:25rem;">
+    let tarjeta = ""
+    let tarjetas = arrayDatos.forEach(event => {tarjeta += `<div class="card" style="width: 18rem; height:25rem;">
     <img src="${event.image}" class="card-img-top" alt="cardImg" width="150" height="150">
     <div class="card-body">
         <h5 class="card-title">${event.name}</h5>
@@ -26,33 +14,24 @@ function crearTarjetas(arrayDatos){
         <p>$${event.price}</p>
         <a href="details.html" class="btn btn-dark" id="btn">See more</a>
     </div>
-</div> `
-        
+</div> `    
     });
-    return tarjetas
+    return tarjeta
 }
+
 /*
 console.log(Object.keys(data))
 console.log(Object.keys(data.events))
 console.log(data.events)
 console.log(data.events[1])
 console.log(Object.values(data.events));
-
+*/
 const contenedorFiltros = document.querySelector('#contenedorFiltros')
 
-    let categorias
-    let events = data.events
-    
-    events.map(event => categorias += ' ' + event.category)
-console.log(categorias);
-console.log(typeof(categorias));
-console.log(event);
-*/
-//let events = data.events
-//let event = events.catego
-let category = data.events.map((event) => event.category)
-;
-console.log(category);
+function nombreCategorias(arrayDatos){
+let category = arrayDatos.map((event) => event.category)
 categorias = new Set(category)
+return categorias
+}
+categorias = nombreCategorias(data.events)
 console.log(categorias);
-
--- scripts/upEvents.js
+++ scripts/upEvents.js
@@ -1,23 +1,20 @@
-const contenedorTarjetas = document.querySelector('#contenedorCards')
-
-let tarjetasCreadas = crearTarjetas(data.events)
-
-contenedorTarjetas.innerHTML = tarjetasCreadas
-
-function crearTarjetas(arrayDatos){
-    let tarjetas = ''
-    for(const event of arrayDatos){
-        if(event.date > data.currentDate){
-        tarjetas += `<div class="card" style="width: 18rem; height:25rem;">
-        <img src="${event.image}" class="card-img-top" alt="cardImg" width="150" height="150">
-        <div class="card-body">
-            <h5 class="card-title">${event.name}</h5>
-            <p class="card-text">${event.description}</p>
-            <p>$${event.price}</p>
-            <a href="details.html" class="btn btn-dark" id="btn">See more</a>
-        </div>
-    </div> `
+let eventosFiltrados = data.events.filter(event => event.date > data.currentDate )
+function tarjetasFuturas(arrayDatos){
+    let tarjeta = ""
+    let eventosFiltrados = arrayDatos.filter(event => event.date > data.currentDate )
+    crearTarjetas(arrayDatos)
+    /*let tarjetasFiltradas = arrayDatos.forEach(event => {tarjeta += `<div class="card" style="width: 18rem; height:25rem;">
+    <img src="${event.image}" class="card-img-top" alt="cardImg" width="150" height="150">
+    <div class="card-body">
+        <h5 class="card-title">${event.name}</h5>
+        <p class="card-text">${event.description}</p>
+        <p>$${event.price}</p>
+        <a href="details.html" class="btn btn-dark" id="btn">See more</a>
+    </div>
+</div> `    
+    });*/
+    return tarjeta
 }
-    }
-    return tarjetas
-}
+let eventoFuturo = tarjetasFuturas(eventosFiltrados)
+console.log(eventosFiltrados);
+console.log(eventoFuturo);
