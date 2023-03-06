0deeb04aee6f091f8385824769e0ee7c4bb59094
LuuGolis <golischevskyl@gmail.com>
2023-03-06 18:14:57 -0300

    Avances en checkbox dinámicas

--- scripts/index.js
+++ scripts/index.js
@@ -27,11 +27,34 @@ console.log(data.events[1])
 console.log(Object.values(data.events));
 */
 const contenedorFiltros = document.querySelector('#contenedorFiltros')
+const checkBox = document.querySelectorAll("input[type=checkBox]")
+const label = document.querySelectorAll("label")
 
 function nombreCategorias(arrayDatos){
-let category = arrayDatos.map((event) => event.category)
-categorias = new Set(category)
-return categorias
+    let category = arrayDatos.map((event) => event.category)
+    categorias = new Set(category)
+    return categorias
 }
 categorias = nombreCategorias(data.events)
 console.log(categorias);
+console.log(typeof(categorias));
+console.log(categorias.values());
+
+categorias.forEach(function(value) {
+    console.log(value)
+  })
+/*
+let checkboxCreados = crearCheckBox(categorias)
+contenedorFiltros.innerHTML = checkboxCreados
+
+function crearCheckBox(arrayCategorias){
+ let etiqueta = ''
+ let category = 
+ let labels =  arrayCategorias.forEach(label=>{
+    etiqueta += `<label>
+    ${category}
+    <input type="checkbox" name="" value="">
+</label>`
+ })
+}
+*/
