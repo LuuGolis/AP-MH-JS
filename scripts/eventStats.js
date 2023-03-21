dfa8e5daf52bb49e2560ad6be76a7d86062db25e
LuuGolis <golischevskyl@gmail.com>
2023-03-21 11:32:17 -0300

    Script stats fin

--- scripts/eventStats.js
+++ scripts/eventStats.js
@@ -2,55 +2,112 @@ let urlApi = "https://mindhub-xj03.onrender.com/api/amazing"
 const contenedorTarjetas = document.querySelector('#contenedorCards')
 let eventos = []
 
-const stats = document.getElementById('eventStats')
-const upStats = document.getElementById('upEventsStats')
-const pastStats = document.getElementById('pastEventsStats')
-
 function traerDatos() {
     fetch(urlApi).then(response => response.json())
         .then(datosAPI => {
             eventos = datosAPI.events
-            //ordenar de mayor asistencia a menor
-           let eventosF = eventos.filter(event => event.date <= datosAPI.currentDate)
-           let  e = ordenAsc(eventosF)
-           let f = ordenDes(eventosF)
-        console.log(e);
-        console.log(f);
-               dibujarTabla(e,stats)
+            arrayPast = pastEvents(eventos, datosAPI.currentDate)
+            arrFuture = futureEvents(eventos, datosAPI.currentDate)
+
+            printTable1(results(assistance(eventos), assistance(eventos).reverse(), capacity(eventos)), "eventStats")
+
+            printTable(dataTable(arrFuture), "upEventsStats")
+            printTable(dataTable(arrayPast), "pastEventsStats")
         })
         .catch(error => console.log(error.message))
 }
 
 traerDatos()
 
-function filtrar(datos){
-    const datosFiltrados = datos.filter(event => event.date <= eventos.currentDate)
-    return console.log(datosFiltrados);
-}
-filtrar(eventos)
-
-function ordenAsc(array){    
-    const sorted = [...array].sort((a,b) => a.assistance - b.assistance)
-    return  sorted.map(event => event.name)
+function futureEvents(array, date) {
+    return array.filter(event => event.date > date)
 }
 
-function ordenDes(array){
-     const sorted = [...array].sort((a,b) => b.assistance - a.assistance)
-     return sorted.map(event => event.name)
+function pastEvents(array, date) {
+    return array.filter(event => event.date < date)
 }
 
-function dibujarTabla(array, lugar){
-    array.forEach(evento =>{
-    let tbItem = document.createElement("tr")
-    tbItem.innerHTML = evento
+function assistance(array) {
+    const percentage = array.map(event => {
+        return {
+            attendance: (event.assistance / event.capacity) * 100,
+            nameEvent: event.name
+        }
+    })
+    percentage.sort((a, b) => b.attendance - a.attendance)
+    //console.log(percentage)
+    return percentage
+}
 
-    lugar.appendChild(tbItem)
-})
-    /*let tableItem = ""
-    let item = array.forEach(eventos =>{
-        tableItem += `<tr>
-        <td>${eventos.name}</td>
-        <td>${eventos.name}</td>
-      </tr>`})*/
-      lugar.innerText = item
+function capacity(arr) {
+    const arrCapacity = arr.map(event => {
+        return {
+            capacity: event.capacity,
+            nameEvent: event.name
+        }
+    })
+    arrCapacity.sort((a, b) => b.capacity - a.capacity)
+   // console.log(arrCapacity)
+    return arrCapacity
+}
+
+function results(highestPercentage, lowestPercentage, largerCapacity) {
+    let all = {
+        highestPercentage: highestPercentage[0].nameEvent,
+        lowestPercentage: lowestPercentage[0].nameEvent,
+        largerCapacity: largerCapacity[0].nameEvent
+    }
+    return all
+}
+
+function printTable1(results, container) {
+    const table = document.getElementById(container)
+    table.innerHTML = `
+    <tr>
+        <td>${results.highestPercentage}</td>
+        <td>${results.lowestPercentage}</td>
+        <td>${results.largerCapacity}</td>
+    </tr>
+    `
+}
+
+function dataTable(array) {
+    let categories = Array.from(new Set(array.map(a => a.category)))
+    //console.log(categories)
+
+    let eventCategories = categories.map(category =>
+        array.filter(event => event.category == category))
+    //console.log(eventCategories);
+
+    let result = eventCategories.map(eventCat => {
+        let calculate = eventCat.reduce((acc, event) => {
+            acc.category = event.category;
+            acc.revenues += event.price * (event.assistance || event.estimate);
+            acc.attendance += ((event.assistance || event.estimate) * 100) / event.capacity
+            return acc
+        },
+            {
+                category: "",
+                revenues: 0,
+                attendance: 0
+            })
+        calculate.attendance = calculate.attendance / eventCat.length
+        return calculate
+    })
+    return result
+
+}
+
+function printTable(array, idTag) {
+    const upcomingTable = document.getElementById(idTag)
+    let html = array.map(events => {
+        return `
+        <tr>
+                <td>${events.category}</td>
+                <td>$${events.revenues}</td>
+                <td>${events.attendance.toFixed(2)}%</td>
+            </tr>
+        `
+    })
+    upcomingTable.innerHTML = html.join("")
 }
