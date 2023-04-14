7b8ecb7e24305795d539d6cde8608d8da43e023a
LuuGolis <golischevskyl@gmail.com>
2023-04-14 11:11:40 -0300

    Upcoming Events con Vue

--- scripts/upEvents.js
+++ scripts/upEvents.js
@@ -1,4 +1,81 @@
-let urlApi = "https://mindhub-xj03.onrender.com/api/amazing"
+
+const { createApp } = Vue
+
+createApp({
+    data() {
+        return {
+            urlApi: 'https://mindhub-xj03.onrender.com/api/amazing',
+            fecha: '',
+            eventos: [],
+            eventosBackUp: [],
+            eventosFuturos:[],
+            texto: '',
+            categorias:[],
+            categoriaSeleccionada:[],
+        }
+    },
+    created() {
+        this.traerDatos()
+
+    },
+    mounted() {
+
+    },
+    methods: {
+        traerDatos() {
+            fetch(this.urlApi)
+                .then(response => response.json())
+                .then(datos => {
+                    this.eventos = datos.events
+                    this.fecha = datos.currentDate
+                    this.eventosFuturos = this.eventos.filter(evento => evento.date > this.fecha)
+                    this.eventosBackUp = this.eventosFuturos
+
+                    this.extraerCategorias(this.eventos);
+                })
+                .catch(error => console.log(error.message))
+        },
+        extraerCategorias(array){
+            array.forEach(elemento =>{
+                if(!this.categorias.includes(elemento.category))
+                this.categorias.push(elemento.category)
+            })
+            console.log(this.categorias);
+        }
+
+    },
+    computed: {
+/*
+        filtroTexto(){
+            this.eventos = this.eventosBackUp.filter(evento => evento.name.toLowerCase().includes(this.texto.toLowerCase())
+            || evento.description.toLowerCase().includes(this.texto.toLowerCase()))
+        },
+        filtroCategoria(){
+            if(this.categoriaSeleccionada.length>0){
+                this.eventos = this.eventosBackUp.filter(evento => this.categoriaSeleccionada.
+                    includes(evento.category))
+            }
+            else{
+                this.eventos = this.eventosBackUp
+            }
+
+        },*/
+
+        filtros() {
+            let filtroTexto = this.eventosBackUp.filter(evento => evento.name.toLowerCase().includes(this.texto.toLowerCase())
+            || evento.description.toLowerCase().includes(this.texto.toLowerCase()))
+            if(this.categoriaSeleccionada.length>0){
+                this.eventos = filtroTexto.filter(evento => this.categoriaSeleccionada.includes(evento.category))
+            }else{
+                this.eventos = filtroTexto
+            }
+        }
+    },
+}).mount('#app')
+
+
+
+/*let urlApi = "https://mindhub-xj03.onrender.com/api/amazing"
 
 const contenedorTarjetas = document.querySelector('#contenedorCards')
 let eventos = []
@@ -83,3 +160,4 @@ function filtros() {
         contenedorTarjetas.innerHTML = "Oooops! La búsqueda no arrojó resultados :("
     }
 }
+¨*/
